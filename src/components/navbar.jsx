ee75bcd8a0340745901fd7431ee3bd228a4bfcf3
James Carter <carterjamesmike@gmail.com>
2023-02-09 15:20:35 -0600

    Added mobile menu for navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,26 +2,29 @@ import React, { useState } from "react";
 import {FaBars, FaTimes} from "react-icons/fa";
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav
+        )
     return (
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
 
-                <ul className="hidden md:flex">
-                    <li>Home</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                </ul>
+            <ul className="hidden md:flex">
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
 
 
             {/* Hamburger */}
-            <div className="md:hidden z-10">
-                <FaBars />
+            <div onClick={handleClick} className="md:hidden z-10">
+                {!nav ? <FaBars /> : <FaTimes /> }
             </div>
 
             {/* Mobile Menu */}
-            <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
                 <li className="py-6 text-4xl">Home</li>
                 <li className="py-6 text-4xl">Projects</li>
-                <li className="py-6 text-4xl">Contact</li>
+             <li className="py-6 text-4xl">Contact</li>
             </ul>
 
             {/* Social Icons */}
