bf2fa355bdd979eace0b119cc3121d86253d8b40
James Carter <carterjamesmike@gmail.com>
2023-02-08 23:35:54 -0600

    Added styling and some responsiveness to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import {FaBars, FaTimes} from "react-icons/fa";
 
 const Navbar = () => {
     return (
-        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]">
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
 
+                <ul className="hidden md:flex">
+                    <li>Home</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+
+
+            {/* Hamburger */}
+            <div className="md:hidden z-10">
+                <FaBars />
+            </div>
+
+            {/* Mobile Menu */}
+            <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+                <li className="py-6 text-4xl">Home</li>
+                <li className="py-6 text-4xl">Projects</li>
+                <li className="py-6 text-4xl">Contact</li>
+            </ul>
+
+            {/* Social Icons */}
+            <div className="hidden"></div>
         </div>
     )
 }
