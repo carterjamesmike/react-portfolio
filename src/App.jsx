25e5e84ac7bec81d7dae37146e53174836c73f3b
James Carter <carterjamesmike@gmail.com>
2023-02-10 10:52:58 -0600

    Added about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   )
 }
