9b2beaccc40d328921284749a926b690b84b4a7e
James Carter <carterjamesmike@gmail.com>
2023-02-12 20:57:15 -0600

    Linked contact form to formspark and used their field validations

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,50 @@ import { validateEmail } from '../utils/helpers';
 
 const Contact = () => {
 
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue)
+    } else {
+      setMessage(inputValue)
+    }
+
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)) {
+      setErrorMessage('Please enter a valid email');
+      return;
+    }
+    if (!name) {
+      setErrorMessage('Please enter a name or organization')
+      return;
+    }
+    if (!message) {
+      setErrorMessage('Please enter a message');
+      return;
+    };
+
+    setEmail('');
+    setName('');
+    setMessage('');
+    setErrorMessage('')
+
+
+  };
+
 
   return (
     <div name='Contact' className='w-full h-screen primary-bg flex justify-center items-center p-4'>
@@ -11,12 +55,18 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 accent-color text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit form below</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name or Organization' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Send</button>
+            <input onChange={handleInputChange} value={name} className='bg-[#ccd6f6] p-2' type="text" placeholder='Name or Organization' name='name' required />
+            <input onChange={handleInputChange} value={email} className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' required/>
+            <textarea onChange={handleInputChange} value={message} className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message' required></textarea>
+            <button type='submit' className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Send</button>
         </form>
+        {errorMessage && (
+        <div>
+          <p className="text-xl text-white">{errorMessage}</p>
+        </div>
+      )}        
     </div>
+    
   )
 }
 
